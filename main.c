03a7e53ac5ab3d0e216b5913c04a5cb6d047508a
AgustinRoca <40873168+AgustinRoca@users.noreply.github.com>
2018-07-06 21:19:05 -0300

    cambio de variable year a string

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MIN_YEAR 2014
-#define MAX_YEAR 2018
+#define MIN_YEAR "2014"
+#define MAX_YEAR "2018"
 
 
 // Testing
@@ -15,9 +15,9 @@ main(int argc, const char *argv[])
 		printf("Error: Ingresar solo un argumento\n");
 		return 1;
 	}
-	short year;
-	year=atoi(argv[1]);
-	if(year<MIN_YEAR || year>MAX_YEAR)
+	char year[5];
+	strncpy(year, arcgv[1], 4);
+	if(strcmp(year, MIN_YEAR)<0 || strcmp(year, MAX_YEAR)>0)
 	{
 		printf("Error: Ingresar un año que esté entre %d y %d\n", MIN_YEAR, MAX_YEAR);
 		return 2;
