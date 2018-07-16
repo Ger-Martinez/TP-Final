8422394d3832676e5dc6fe46440a476299ed004d
AgustinRoca <40873168+AgustinRoca@users.noreply.github.com>
2018-07-16 17:34:11 -0300

    Queries

--- queries.c
+++ queries.c
@@ -7,13 +7,86 @@
 void query1(FILE * resp, airportADT airports)
 {
 	tAirport aux;
-	toBegin(airports);
-	while(hasNext(airports))
+	toBeginAirport(airports);
+	while(hasNextAirport(airports))
 	{
+		aux=nextAirport(airports);
 		if(aux->landings + aux->takeOffs)
+			fprintf(resp, "%s;%s;%s;%u\n", aux->oaci, aux->local, aux->name, (aux->takeOffs + aux->landings));		
+	}
+}
+
+void query2(FILE * resp, airportADT airports)
+{
+	tAirport aux;
+	toBeginAirport(airports);
+	while(hasNextAirport(airports))
+	{
+		aux=nextAirport(airports);
+		if(aux->isInternational && (aux->landings + aux->takeOffs))
+			fprintf(resp, "%s;%s;%u;%u;%u\n", aux->oaci, aux->iata, aux->takeOffs, aux->landings, aux->landings + aux->takeOffs);
+	}
+}
+
+void query3(FILE * resp, airportADT airports)
+{
+	unsigned int days[7]=getDayVec(airports);
+	fprintf(resp, "Lunes;%u\n", days[1]);
+	fprintf(resp, "Martes;%u\n", days[2]);
+	fprintf(resp, "Miercoles;%u\n", days[3]);
+	fprintf(resp, "Jueves;%u\n", days[4]);
+	fprintf(resp, "Viernes:%u\n", days[5]);
+	fprintf(resp, "Sabado;%u\n", days[6]);
+	fprintf(resp, "Domingo;%u\n", days[0]);
+}
+
+void query4(FILE * resp, airportADT airports)
+{
+	tAirport aux;
+	tDestination dest;
+	toBeginAirport(airports);
+	while(hasNextAirport(airports))
+	{
+		aux=nextAirport(airports);
+		toBeginDestinations(aux->destinations);
+		while(hasNextDestination(aux->destinations))
 		{
-			aux=nextAirport(airports);
-			fprintf(resp, "%s;%s;%s;%d\n", aux->oaci, aux->local, aux->name, (aux->takeOffs + aux->landings));		
+			dest=nextDestination(aux->destinations);
+			fprintf(resp, "%s;%s;%u;%u\n", aux->oaci, dest->oaci, dest->landings, dest->takeOffs);
 		}
 	}
+}
+
+void query5(FILE * resp, airportADT airports)
+{
+	tAirport aux;
+	toBeginAirport(airports);
+	total=getTotal(airports);
+	while(hasNextAirport(airports))
+	{
+		aux=nextAirport(airports);
+		if(aux->isInternational && (aux->landings + aux->takeOffs))
+		{
+			fprintf(resp, "%s;%s;%u;%u;%u;%.2f\n", aux->oaci, aux->iata, aux->takeOffs, aux->landings, aux->landings + aux->takeOffs, (aux->landings + aux->takeOffs)*100/(float)total);
+		}
+	}
+}
+
+void query6(FILE * resp, airlinesADT airlines)
+{
+	tAirline aux;
+	toBeginAirlines(airlines);
+	while(hasNextAirport(airlines))
+	{
+		aux=nextAirline(airlines);
+		fprintf(resp, "%s;%u\n", aux->name, aux->movs);
+	}
+}
+
+void query7(FILE * resp, airportADT airports)
+{
+	fprintf(resp, "Aterrizaje;Cabotaje;%u\n", );
+	fprintf(resp, "Aterrizaje;Internacional;%u\n", );
+	fprintf(resp, "Despegue;Cabotaje;%u\n", );
+	fprintf(resp, "Despegue;Internacional;%u\n", );
 }
