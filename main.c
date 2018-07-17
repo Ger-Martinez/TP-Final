1f9e42c674219a11ccdf1ff2866bb4e6d8c237db
Tanis016 <40873179+Tanis016@users.noreply.github.com>
2018-07-17 03:12:12 -0300

    grtfd

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include "airportADT.h"
 #include "destinationADT.h"
 #include "airlineADT.h"
+#include "queries.h"
 #define SIZE_FIRST_LINE_A 184
 #define SIZE_FIRST_LINE_F 144
 #define CANT_AEROPUERTOS 5
@@ -32,10 +33,36 @@ main (void)
 
     /* Comienzo a trabajar */
 
-    airportADT ap = newAirportList();
+    airportADT airport = newAirportList();
     internationalADT inter = newInternationalList();
-    addAirports( ap , archA );
-    addMovements( ap , inter , archF );
+    addAirports( airport , archA );
+    addMovements( airport , inter , archF );
+
+		FILE *query1F = fopen("movs_aeropuerto.csv", "w");
+		FILE *query2F = fopen("movs_internacional.csv", "w");
+		FILE *query3F = fopen("semanal.csv", "w");
+		FILE *query4F = fopen("aerop_detalle.csv", "w");
+		FILE *query5F = fopen("movs_internacion5.csv", "w");
+		FILE *query6F = fopen("aerolinea.csv","w");
+		FILE *query7F = fopen("movimiento.csv", "w");
+
+		query1(query1F, airport);
+		query2(query2F, airport);
+		query3(query3F, airport);
+		query4(query4F, airport);
+		query5(query5F, inter  );
+		query6(query6F, airport);
+		query7(query7F, airport);
+
+		fclose(query1F);
+		fclose(query2F);
+		fclose(query3F);
+		fclose(query4F);
+		fclose(query5F);
+		fclose(query6F);
+		fclose(query7F);
+
+
 
 
 	return 0;
