7b15cd52e8b35d201b8af32799417dd03b1e8513
Tanis016 <40873179+Tanis016@users.noreply.github.com>
2018-07-16 22:50:48 -0300

    q

--- queries.h
+++ queries.h
@@ -1,3 +1,5 @@
+#ifndef _QUERIES_H_
+#define _QUERIES_H_
 void query1(FILE * resp, airportADT airports);
 
 void query2(FILE * resp, airportADT airports);
@@ -11,3 +13,5 @@ void query5(FILE * resp, airportADT airports);
 void query6(FILE * resp, airlineQueryADT airlines);
 
 void query7(FILE * resp, airportADT airports);
+
+#endif
